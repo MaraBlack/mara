fa5ef9926dd98e512c94e6319ebdc29e5122123c
Mara Black <me.mara.black@outlook.com>
2024-02-13 21:46:36 +0200

    [dev] unsaved files :(

--- src/app/shared/misc/utils.ts
+++ src/app/shared/misc/utils.ts
@@ -18,14 +18,14 @@ export function adjustColor(color: THREE.Color, amount: number) {
 export function enumToMap(enumeration: any): Map<string, string | number> {
   const map = new Map<string, string | number>();
   for (let key in enumeration) {
-      //TypeScript does not allow enum keys to be numeric
-      if (!isNaN(Number(key))) continue;
+    //TypeScript does not allow enum keys to be numeric
+    if (!isNaN(Number(key))) continue;
 
-      const val = enumeration[key] as string | number;
+    const val = enumeration[key] as string | number;
 
-      //TypeScript does not allow enum value to be null or undefined
-      if (val !== undefined && val !== null)
-          map.set(key, val);
+    //TypeScript does not allow enum value to be null or undefined
+    if (val !== undefined && val !== null)
+      map.set(key, val);
   }
 
   return map;
